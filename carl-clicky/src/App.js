be9a22049463f45670c304a14d932a6acb0f26bd
Carl Medina <carl.l.medina@comcast.net>
2019-11-22 10:02:31 -0800

    made folder and file structure a little better looking

--- carl-clicky/src/App.js
+++ carl-clicky/src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
-import Clicky from "./components/Clicky";
+import Wrapper from "./components/Wrapper/index";
+import Navbar from "./components/Navbar/index";
+import Header from "./components/Header/index";
+import Main from "./components/Main/index";
+import Footer from "./components/Footer/index";
 
 function App() {
-  return <Clicky />;
+  return (
+    <Wrapper>
+      <Navbar />
+      <Header />
+      <Main />
+      <Footer />
+    </Wrapper>
+  );
 }
 
 export default App;
